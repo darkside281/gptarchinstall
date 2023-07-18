444e73b12facbfe0e5cb9348be1a8f8c48c68c87
darkside281 <dsavage281@gmail.com>
2023-07-18 22:31:32 +0100

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,131 +1,91 @@
 #!/bin/bash
 
-# Function to print messages in bold
-function bold_echo {
-  echo -e "\033[1m$1\033[0m"
-}
+# Prompt for the username and password
+read -p "Enter the desired username: " username
+read -s -p "Enter the password for the user '$username': " password
+echo
+read -s -p "Confirm password: " password_confirm
+echo
 
-# Function to list available disks and prompt for selection
-function select_disk {
-  disks=($(lsblk -d -n -o NAME))
-  bold_echo "Available disks:"
-  for ((i = 0; i < ${#disks[@]}; i++)); do
-    echo "$i. /dev/${disks[i]}"
-  done
-  read -p "Enter the number of the disk to use (e.g., 0, 1, 2, ...): " disk_number
-  selected_disk="/dev/${disks[disk_number]}"
-}
-
-# Function to create partitions on the selected disk
-function create_partitions {
-  parted -s $selected_disk mklabel gpt
-  parted -s $selected_disk mkpart primary 1MiB 512MiB   # EFI System Partition (ESP)
-  parted -s $selected_disk set 1 boot on
-  parted -s $selected_disk mkpart primary 512MiB 100%   # Root partition
-}
-
-# Prompt for Desktop Environment Selection
-bold_echo "Choose Desktop Environment:"
-bold_echo "1. GNOME"
-bold_echo "2. KDE"
-bold_echo "3. XFCE"
-read -p "Enter your choice (1/2/3): " desktop_choice
-
-# Prompt and select the disk for installation
-select_disk
-
-# Create partitions on the selected disk
-bold_echo "Automatically partitioning the disk..."
-create_partitions
-
-# Format partitions
-mkfs.vfat -F32 "${selected_disk}1"
-mkfs.btrfs "${selected_disk}2"
-
-# Mount the root partition
-mount "${selected_disk}2" /mnt
-
-# Create subvolumes for root partition
-btrfs su cr /mnt/@
-btrfs su cr /mnt/@home
-btrfs su cr /mnt/@var
-
-# Mount the subvolumes
-umount /mnt
-mount -o noatime,compress=zstd,space_cache=v2,subvol=@ "${selected_disk}2" /mnt
-mkdir -p /mnt/{boot/efi,home,var}
-mount -o noatime,compress=zstd,space_cache=v2,subvol=@home "${selected_disk}2" /mnt/home
-mount -o noatime,compress=zstd,space_cache=v2,subvol=@var "${selected_disk}2" /mnt/var
-
-# Install the base system and necessary packages
-pacstrap /mnt base base-devel linux linux-firmware btrfs-progs sudo grub networkmanager
-
-# Generate fstab
-genfstab -U /mnt >> /mnt/etc/fstab
-
-# Chroot into the new system
-arch-chroot /mnt /bin/bash <<EOF
-
-# Prompt for setting the timezone
-bold_echo "Set your timezone:"
-bold_echo "1. London"
-bold_echo "2. Other (You will need to provide the timezone path)"
-read -p "Enter your choice (1/2): " timezone_choice
-
-if [ "\$timezone_choice" == "1" ]; then
-  ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
-else
-  # Uncomment and set the desired timezone path
-  # ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
-  # Example: ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
-  bold_echo "Please set the timezone manually by editing /etc/localtime."
+# Check if the passwords match
+if [[ "$password" != "$password_confirm" ]]; then
+  echo "Passwords do not match. Please run the script again."
+  exit 1
 fi
 
-hwclock --systohc --utc
+# Prompt for the disk to use for installation
+lsblk # Show available disks
+read -p "Enter the disk to use for installation (e.g., /dev/sda): " target_disk
 
-# Set the locale to UK
-echo "en_GB.UTF-8 UTF-8" > /etc/locale.gen
-locale-gen
-echo "LANG=en_GB.UTF-8" > /etc/locale.conf
+# Prompt for the desktop environment
+echo "Available desktop environments: GNOME, KDE, XFCE, LXDE, Cinnamon"
+read -p "Enter the desktop environment to install: " desktop_environment
 
-# Set the keyboard layout to UK
-echo "KEYMAP=uk" > /etc/vconsole.conf
+# Update system clock
+timedatectl set-ntp true
+
+# Wipe the target disk and create partitions
+# WARNING: This will delete all data on the specified disk. Make sure you've backed up your data.
+# You may use tools like parted, fdisk, or cfdisk for partitioning. Example:
+# parted -s "$target_disk" mklabel gpt
+# parted -s "$target_disk" mkpart primary ext4 1MiB 100%
+
+# Format partitions and mount the root partition
+# mkfs.ext4 /dev/sdX1 # Replace sdX1 with the correct partition
+# mount /dev/sdX1 /mnt # Replace sdX1 and /mnt with the correct partition and mount point
+
+# Install essential packages and base system
+# pacstrap /mnt base linux linux-firmware
+
+# Generate fstab file
+# genfstab -U /mnt >> /mnt/etc/fstab
+
+# Change root into the new system
+# arch-chroot /mnt
+
+# Set the time zone
+# ln -sf /usr/share/zoneinfo/Your_Region/Your_City /etc/localtime
+# hwclock --systohc
+
+# Uncomment the desired locale in /etc/locale.gen
+# Generate the locale
+# locale-gen
+
+# Set the system language
+# echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 # Set the hostname
-read -p "Enter your desired hostname: " hostname
-echo "\$hostname" > /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 \$hostname.localdomain \$hostname" >> /etc/hosts
+# echo "your_desired_hostname" > /etc/hostname
 
-# Install and configure GRUB bootloader
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub
-grub-mkconfig -o /boot/grub/grub.cfg
+# Add matching entries to /etc/hosts
+# Example: 127.0.0.1 localhost
+#          ::1       localhost
+#          127.0.1.1 your_desired_hostname.localdomain your_desired_hostname
 
-# Install GNOME Desktop Environment
-case "$desktop_choice" in
-  1) # GNOME
-    pacman -S gnome gnome-extra
-    ;;
-  2) # KDE
-    pacman -S plasma kde-applications
-    ;;
-  3) # XFCE
-    pacman -S xfce4 xfce4-goodies
-    ;;
-  *) # Default to GNOME
-    pacman -S gnome gnome-extra
-    ;;
-esac
+# Install and configure network manager
+# pacman -S networkmanager
+# systemctl enable NetworkManager
 
-# Prompt for creating a user with sudo access
-read -p "Enter a username for the new user: " username
-useradd -m -G wheel \$username
-passwd \$username
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+# Install bootloader (GRUB)
+# pacman -S grub
+# grub-install --target=i386-pc /dev/sdX # Replace sdX with the disk (not the partition)
+# grub-mkconfig -o /boot/grub/grub.cfg
 
-EOF
+# Create a user with sudo access
+# useradd -m -G wheel "$username"
+# echo "$username:$password" | chpasswd
 
-# Unmount all partitions and reboot
-umount -R /mnt
-reboot
+# Configure sudo
+# Uncomment the %wheel ALL=(ALL) ALL line in /etc/sudoers to allow members of the wheel group to use sudo
+
+# Install and configure the desktop environment
+# The packages and configuration will depend on the chosen desktop environment (GNOME, KDE, XFCE, etc.)
+
+# Enable and start the display manager (e.g., GDM for GNOME, SDDM for KDE)
+# systemctl enable gdm # Replace gdm with the appropriate display manager
+
+# Exit the chroot environment and unmount partitions
+# exit
+# umount -R /mnt
+
+echo "Arch Linux installation completed successfully!"
